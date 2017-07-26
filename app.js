d777e307e28940200b63a93b31a0bc6a158dd6df
HarpalSinh Chauhan <harpal.hybris@gmail.com>
2017-07-25 23:33:27 -0700

    working mongodb

--- app.js
+++ app.js
@@ -1,68 +1,94 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var stylus = require('stylus');
-var morgan = require('morgan');
-
-//config file for mongodb
-var config = require('./bin/config');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
+// mongoose.connect('mongodb://localhost/userdb', function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("connected to mongodb");
+//   }
+// });
 
-var index = require('./routes/index');
+mongoose.connect('mongodb://localhost/userdb', { useMongoClient: true });
+var db = mongoose.connection;
+
+
+var routes = require('./routes/index');
 var users = require('./routes/users');
-var main = require('./routes/main');
-var login = require('./routes/login');
-var signup = require('./routes/signup');
+
+// Init App
 var app = express();
 
-var api = require('./bin/server-api');
-app.use('/api',api);
-mongoose.connect(config.database, function(err){
-	if(err){
-		console.log(err);
-	}else{
-		console.log("connected to mongodb");
-	}
-})
-// view engine setup
+// View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
+
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+
+
+app.use('/', routes);
 app.use('/users', users);
-app.use('/main', main);
-app.use('/login', login);
-app.use('/signup',signup);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+// Set Port
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+  console.log('Server started on port '+app.get('port'));
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
--- models/user.js
+++ models/user.js
@@ -1,25 +1,53 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var bcrypt = require('bcrypt-nodejs');
-var UserSchema = new Schema({
-	name:String,
-	username:{type:String, required:true, index:{unique:true}},
-	password:{type:String, required:true, select:false}
-
-});
-UserSchema.pre('save',function(next){
-	var user = this;
-	if(!user.isModified('password')) return next();
-	bcrypt.hash(user.password, null , null, function(err,hash){
-		if(err) return next(err);
-		user.password = hash;
-		next();
-	});
-});
-
-UserSchema.methods.comparePassword= function(password){
-	var user = this;
-	return bcrypt.compareSync(password, user.password);
+var bcrypt = require('bcryptjs');
+mongoose.Promise = global.Promise;
+// User Schema
+var UserSchema = mongoose.Schema({
+	username: {
+		type: String,
+		index:true
+	},
+	email: {
+		type: String
+	},
+	password: {
+		type: String
+	}
 	
+});
+// UserSchema.pre('save',function(next){
+// 	var user = this;
+// 	if(!user.isModified('password')) return next();
+// 	bcrypt.hash(user.password, null , null, function(err,hash){
+// 		if(err) return next(err);
+// 		user.password = hash;
+// 		next();
+// 	});
+// });
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
 }
-module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserByUsername = function(username, callback){
+	var query = {username: username};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+}
